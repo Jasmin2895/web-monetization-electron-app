eab8c5743eeec05007ee719642611331f9bc9905
buckleader <jasmin28virdi@gmail.com>
2020-05-09 22:55:19 +0530

    app files restructured and index.html file added

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ app.on("ready", ()=> {
         width:300,
         height:600,
         show: false
-    })
-
+    });
     mainWindow.once("ready-to-show", ()=> {
         mainWindow.show();
-    })
+    });
 
     mainWindow.on("closed", ()=> {
         mainWindow= null;
-    })
+    });
 
     mainWindow.loadURL("https://coil.com/");
-})
+});
