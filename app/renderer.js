da9972f21003e3b60d05d029fb202e2ca710728e
buckleader <jasmin28virdi@gmail.com>
2020-05-15 23:20:27 +0530

    remove comments

--- app/renderer.js
+++ app/renderer.js
@@ -1,16 +1,3 @@
-// const os = require("os");
-// const fs = require("fs");
-
-// const files = fs.readdirSync(os.homedir());
-
-// console.log(files);
-
-// files.forEach((name) => {
-//   const file = document.createElement("li");
-//   file.textContent = name;
-//   window.document.body.appendChild(file);
-// });
-
 const newLinkUrl = document.querySelector("#new-link-url");
 const newLinkSubmit = document.querySelector(".new-link-form--submit");
 
