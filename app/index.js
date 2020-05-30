a7665d429d2342885a6b7b9ab2be32b102373d1d
buckleader <jasmin28virdi@gmail.com>
2020-05-30 22:17:52 +0530

    folder and files

--- app/index.js
+++ app/index.js
@@ -1,40 +1,43 @@
+// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-let mainWindow = null;
-
-function createAppWindow() {
-  mainWindow = new BrowserWindow({
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
-  mainWindow.once("ready-to-show", () => {
-    mainWindow.show();
-  });
-
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
-
+  // and load the index.html of the app.
   mainWindow.loadFile("index.html");
 
-  mainWindow.webContents.openDevTools();
-  mainWindow.addTabbedWindow();
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
 
-app.whenReady().then(createAppWindow);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createAppWindow();
-  }
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") app.quit();
 });
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
